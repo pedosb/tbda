c8da2c39340995580a0bceb24c428ec9ce398842
Pedro Batista <pedro2@pedro-aspire.(none)>
2011-03-23 14:42:01 +0000

    Havia esquecido de salvar 4.ii

--- trab1/respostas/4.ii.sql
+++ trab1/respostas/4.ii.sql
@@ -1,12 +1,14 @@
-SELECT disciplina_id FROM (
-  SELECT disciplina, COUNT(disciplina) num_resp_com_5
-    FROM (SELECT DISTINCT pergunta_id, disciplina_id
-      FROM ipdw_respostas
-      WHERE resposta  = 5 AND
-        semesetre_id = 21)
-  GROUP BY disciplina)
-  WHERE
-  num_resp_com_5 = (SELECT COUNT(pergunta_id)
-    FROM (SELECT DISTINCT pergunta_id
+SELECT disciplina_id, sigla
+  FROM ipdw_disciplina
+  INNER JOIN (SELECT disciplina_id disciplina_id_com_5 FROM (
+    SELECT disciplina_id, COUNT(disciplina_id) num_resp_com_5
+      FROM (SELECT DISTINCT pergunta_id, disciplina_id
         FROM ipdw_respostas
-        WHERE semestre_id = 21));
+        WHERE resposta  = 5 AND
+          semestre_id = 21)
+    GROUP BY disciplina_id)
+    WHERE num_resp_com_5 = (SELECT COUNT(pergunta_id)
+      FROM (SELECT DISTINCT pergunta_id
+          FROM ipdw_respostas
+          WHERE semestre_id = 21)))
+  ON disciplina_id_com_5 = disciplina_id;
