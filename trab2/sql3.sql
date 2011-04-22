cc36bfc7d4134878e2b12c095dc415d8e4ff61aa
Pedro Batista <pedosb@gmail.com>
2011-04-22 20:23:35 +0000

    Inserts

--- trab2/sql3.sql
+++ trab2/sql3.sql
@@ -1,3 +1,20 @@
+DROP TYPE CATEGORIA FORCE;
+DROP TYPE S_CATEGORIA FORCE;
+DROP TYPE PERIODO FORCE;
+DROP TYPE REPETICAO FORCE;
+DROP TYPE TELEFONE FORCE;
+DROP TYPE S_TELEFONE FORCE;
+DROP TYPE MORADA FORCE;
+DROP TYPE S_MORADA FORCE;
+DROP TYPE TOPICO FORCE;
+DROP TYPE S_TOPICO FORCE;
+DROP TYPE CONTATO FORCE;
+DROP TYPE CALENDARIO FORCE;
+DROP TYPE TAREFA FORCE;
+DROP TYPE MEMO FORCE;
+DROP TABLE topicos;
+DROP TABLE categorias;
+
 ------------------CATEGORIA-------------------------------
 CREATE OR REPLACE TYPE CATEGORIA AS OBJECT(
   nome    VARCHAR(25),
@@ -57,7 +74,7 @@ CREATE OR REPLACE TYPE CALENDARIO UNDER TOPICO(
   , r REPETICAO
 );
 ------------------TAREFA----------------------------------
-CREATE OR REPLACE TYPE TAREFA AS OBJECT(
+CREATE OR REPLACE TYPE TAREFA UNDER TOPICO(
     fim       TIMESTAMP
   , completo  VARCHAR2(1)
   , conteudo  VARCHAR2(255)
@@ -65,8 +82,7 @@ CREATE OR REPLACE TYPE TAREFA AS OBJECT(
 ------------------MEMO------------------------------------
 CREATE OR REPLACE TYPE MEMO UNDER TOPICO(
   conteudo VARCHAR2(255)
-  );
-DROP TYPE MEMO;
+);
 ----------------------------------------------------------
 
 ---------------------TABELAS------------------------------
@@ -80,12 +96,25 @@ NESTED TABLE referencias STORE AS referencias_nested;
 DROP TABLE topicos;
 ----------------------------------------------------------
 
+--###################INSERT's#############################
+---------------------CATEGORIAS---------------------------
+INSERT INTO categorias VALUES ('FEUP', NULL);
+  INSERT INTO categorias VALUES ('SRSI', (SELECT REF(cc) FROM categorias cc WHERE nome = 'FEUP'));
+  INSERT INTO categorias VALUES ('TBDA', (SELECT REF(cc) FROM categorias cc WHERE nome = 'FEUP'));
+INSERT INTO categorias VALUES ('Casa', NULL);
+  INSERT INTO categorias VALUES ('Supermercado', (SELECT REF(cc) FROM categorias cc WHERE nome = 'Casa'));
+  INSERT INTO categorias VALUES ('Outros', (SELECT REF(cc) FROM categorias cc WHERE nome = 'Casa'));
+INSERT INTO categorias VALUES ('Trabalho', NULL);
 
----------------------INSERT's-----------------------------
+SELECT * FROM categorias;
+delete from categorias;
+
+----------------CONTATOS--------------------------------
 INSERT INTO topicos VALUES (CONTATO( 
     'João' -- titulo
   , TIMESTAMP '2010-10-15 12:30:00' -- alteracao
-  , (S_CATEGORIA((SELECT Ref(cc) FROM categorias cc WHERE cc.nome = 'Trabalho'))) -- categorias
+  , S_CATEGORIA((SELECT REF(cc) FROM categorias cc WHERE cc.nome = 'Trabalho'),
+                 (SELECT REF(cc) FROM categorias cc WHERE cc.nome = 'FEUP')) -- categorias
   , NULL -- referencias
   , 'João Abreu' -- nome
   , NULL -- Telefone
@@ -93,22 +122,81 @@ INSERT INTO topicos VALUES (CONTATO(
   , 'ja@example.com' -- email
   , 'http://www.exemple.com/ja' -- url
 ));
-
-select * from topicos;
-update categorias set nome='TT' where nome = 'Trabalho';
-
 INSERT INTO topicos VALUES (CONTATO( 
     'Maria' -- titulo
   , TIMESTAMP '2010-10-15 12:30:00' -- alteracao
-  , NULL -- categorias
+  , S_CATEGORIA((SELECT REF(cc) FROM categorias cc WHERE cc.nome = 'FEUP')) -- categorias
   , NULL -- referencias
   , 'Maria Joana' -- nome
   , NULL -- Telefone
   , NULL -- Morada
   , 'mj@example.com' -- email
   , 'http://www.exemple.com/mj' -- url
-  ));
-  
+));
+----------------TAREFAS---------------------------------
+INSERT INTO topicos VALUES (TAREFA(
+    'Comprar papel' -- titulo
+  , TIMESTAMP '2010-10-15 12:30:00' -- alteracao
+  , S_CATEGORIA((SELECT REF(cc) FROM categorias cc WHERE cc.nome = 'Supermercado')) -- categorias
+  , NULL -- referencias
+  , TIMESTAMP '2010-10-20 12:30:00'
+  , 'N'
+  , NULL
+));
+INSERT INTO topicos VALUES (TAREFA(
+    'Pagar aluguel' -- titulo
+  , TIMESTAMP '2010-10-15 12:30:00' -- alteracao
+  , S_CATEGORIA((SELECT REF(cc) FROM categorias cc WHERE cc.nome = 'Outros')) -- categorias
+  , NULL -- referencias
+  , TIMESTAMP '2010-10-20 12:30:00' 
+  , 'N'
+  , NULL
+));
+INSERT INTO topicos VALUES (TAREFA(
+    'Checar correspondencia' -- titulo
+  , TIMESTAMP '2010-10-15 12:30:00' -- alteracao
+  , S_CATEGORIA((SELECT REF(cc) FROM categorias cc WHERE cc.nome = 'Casa')) -- categorias
+  , NULL -- referencias
+  , TIMESTAMP '2010-10-20 12:30:00' 
+  , 'N'
+  , NULL
+));
+----------------MEMO------------------------------
+INSERT INTO topicos VALUES (MEMO(
+    'Tipo' -- titulo
+  , TIMESTAMP '2010-10-15 12:30:00' -- alteracao
+  , S_CATEGORIA((SELECT REF(cc) FROM categorias cc WHERE cc.nome = 'Casa')) -- categorias
+  , NULL -- referencias
+  , 'Para usar o tipo set em uma tabela tem que usar nested table'
+));
+----------------CALENDARIO------------------------
+INSERT INTO topicos VALUES (CALENDARIO(
+    'Trabalho TBDA' -- titulo
+  , TIMESTAMP '2010-10-15 12:30:00' -- alteracao
+  , S_CATEGORIA((SELECT REF(cc) FROM categorias cc WHERE cc.nome = 'TBDA')) -- categorias
+  , S_TOPICO((SELECT REF(t) FROM topicos t WHERE titulo = 'Tipo'),
+             (SELECT REF(t) FROM topicos t WHERE titulo = 'Maria'))
+  , PERIODO(TIMESTAMP '2010-01-29 00:00:00', TIMESTAMP '2010-01-29 03:00:00')
+  , NULL
+));
+INSERT INTO topicos VALUES (CALENDARIO(
+    'Trabalho SRSI' -- titulo
+  , TIMESTAMP '2010-10-15 12:30:00' -- alteracao
+  , S_CATEGORIA((SELECT REF(cc) FROM categorias cc WHERE cc.nome = 'SRSI')) -- categorias
+  , S_TOPICO((SELECT REF(t) FROM topicos t WHERE titulo = 'João'))
+  , PERIODO(TIMESTAMP '2010-01-29 01:00:00', TIMESTAMP '2010-01-29 05:00:00')
+  , NULL
+));
+INSERT INTO topicos VALUES (CALENDARIO(
+    'Aula TBDA' -- titulo
+  , TIMESTAMP '2010-10-15 12:30:00' -- alteracao
+  , S_CATEGORIA((SELECT REF(cc) FROM categorias cc WHERE cc.nome = 'TBDA')) -- categorias
+  , NULL
+  , PERIODO(TIMESTAMP '2010-01-30 00:00:00', TIMESTAMP '2010-01-30 06:00:00')
+  , NULL
+));
+-------------------------------------------------------------------------------------
+
 INSERT INTO topicos VALUES (CONTATO( 
      -- titulo
   , TIMESTAMP '2010-10-15 12:30:00' -- alteracao
