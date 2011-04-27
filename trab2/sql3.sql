76fb169e00baea3c103a012bcf090840c9ef417e
André Fernandes <andrefernandes6@gmail.com>
2011-04-27 23:56:29 +0100

    começo da formatacao do codigo

--- trab2/sql3.sql
+++ trab2/sql3.sql
@@ -12,128 +12,137 @@ DROP TYPE CONTATO FORCE;
 DROP TYPE CALENDARIO FORCE;
 DROP TYPE TAREFA FORCE;
 DROP TYPE MEMO FORCE;
-DROP TABLE topicos;
-DROP TABLE categorias;
+DROP TABLE TOPICOS;
+DROP TABLE CATEGORIAS;
 
 ------------------CATEGORIA-------------------------------
 CREATE OR REPLACE TYPE CATEGORIA AS OBJECT(
   nome    VARCHAR(25),
-  pai     REF CATEGORIA);
+  pai     REF CATEGORIA
+);
 CREATE OR REPLACE TYPE S_CATEGORIA AS TABLE OF REF CATEGORIA;
 ----------------------------------------------------------
 
------------------PERIODO----------------------------------
+------------------PERIODO---------------------------------
 CREATE OR REPLACE TYPE PERIODO AS OBJECT(
-    inicio  TIMESTAMP
-  , fim     TIMESTAMP);
+  inicio  TIMESTAMP,
+  fim     TIMESTAMP
+);
 ----------------------------------------------------------
 
------------------REPETICAO--------------------------------
+------------------REPETICAO-------------------------------
 CREATE OR REPLACE TYPE REPETICAO AS OBJECT(
-    frequencia  VARCHAR2(7) --CHECK ( frequencia IN ('dia', 'semana', 'mês', 'ano'))
-  , p     PERIODO);
+  frequencia  VARCHAR2(7), --CHECK ( frequencia IN ('dia', 'semana', 'mês', 'ano'))
+  p           PERIODO
+);
 ----------------------------------------------------------
 
------------------TELEFONE---------------------------------
+------------------TELEFONE--------------------------------
 CREATE OR REPLACE TYPE TELEFONE AS OBJECT(
-    numero  VARCHAR2(25)
+  numero  VARCHAR2(25)
 );
 CREATE OR REPLACE TYPE S_TELEFONE AS TABLE OF TELEFONE;
 ----------------------------------------------------------
 
 ------------------MORADA----------------------------------
 CREATE OR REPLACE TYPE MORADA AS OBJECT(
-    rua     VARCHAR2(100)
-  , cidade  VARCHAR2(50)
+  rua     VARCHAR2(100),
+  cidade  VARCHAR2(50)
 );
 CREATE OR REPLACE TYPE S_MORADA AS TABLE OF MORADA;
 ----------------------------------------------------------
 
--------------------TOPICO---------------------------------
+------------------TOPICO----------------------------------
 CREATE OR REPLACE TYPE TOPICO AS OBJECT(
-    titulo      VARCHAR2(25)
-  , alteracao   TIMESTAMP
-  , categorias  S_CATEGORIA
-  , referencias S_TOPICO
+  titulo      VARCHAR2(25),
+  alteracao   TIMESTAMP,
+  categorias  S_CATEGORIA,
+  referencias S_TOPICO
 ) NOT FINAL;
-drop type topico force;
   
 CREATE OR REPLACE TYPE S_TOPICO AS TABLE OF REF TOPICO;
-drop type s_topico force;
--------------------CONTATO--------------------------------
+----------------------------------------------------------
+
+------------------CONTATO---------------------------------
 CREATE OR REPLACE TYPE CONTATO UNDER TOPICO(
-    nome      VARCHAR2(100)
-  , telefones S_TELEFONE
-  , moradas   S_MORADA
-  , email     VARCHAR2(100)
-  , url       VARCHAR2(200)
+  nome      VARCHAR2(100),
+  telefones S_TELEFONE,
+  moradas   S_MORADA,
+  email     VARCHAR2(100),
+  url       VARCHAR2(200)
 );
-------------------CLANEDARIO------------------------------
+----------------------------------------------------------
+
+-------------------CALENDARIO-----------------------------
 CREATE OR REPLACE TYPE CALENDARIO UNDER TOPICO(
-    p PERIODO
-  , r REPETICAO
+  p PERIODO,
+  r REPETICAO
 );
+----------------------------------------------------------
+
 ------------------TAREFA----------------------------------
 CREATE OR REPLACE TYPE TAREFA UNDER TOPICO(
-    fim       TIMESTAMP
-  , completo  VARCHAR2(1)
-  , conteudo  VARCHAR2(255)
+  fim       TIMESTAMP,
+  completo  VARCHAR2(1),
+  conteudo  VARCHAR2(255)
 );
+----------------------------------------------------------
+
 ------------------MEMO------------------------------------
 CREATE OR REPLACE TYPE MEMO UNDER TOPICO(
   conteudo VARCHAR2(255)
 );
 ----------------------------------------------------------
 
----------------------TABELAS------------------------------
-CREATE TABLE categorias OF CATEGORIA;
+------------------TABELAS---------------------------------
+CREATE TABLE CATEGORIAS OF CATEGORIA;
 
-CREATE TABLE topicos OF TOPICO
-NESTED TABLE categorias STORE AS categorias_nested
+CREATE TABLE TOPICOS OF TOPICO
+NESTED TABLE CATEGORIAS STORE AS categorias_nested
 NESTED TABLE referencias STORE AS referencias_nested;
-
-
-DROP TABLE topicos;
 ----------------------------------------------------------
 
---###################INSERT's#############################
----------------------CATEGORIAS---------------------------
-INSERT INTO categorias VALUES ('FEUP', NULL);
-  INSERT INTO categorias VALUES ('SRSI', (SELECT REF(cc) FROM categorias cc WHERE nome = 'FEUP'));
-  INSERT INTO categorias VALUES ('TBDA', (SELECT REF(cc) FROM categorias cc WHERE nome = 'FEUP'));
-INSERT INTO categorias VALUES ('Casa', NULL);
-  INSERT INTO categorias VALUES ('Supermercado', (SELECT REF(cc) FROM categorias cc WHERE nome = 'Casa'));
-  INSERT INTO categorias VALUES ('Outros', (SELECT REF(cc) FROM categorias cc WHERE nome = 'Casa'));
-INSERT INTO categorias VALUES ('Trabalho', NULL);
+--################INSERTs#################################
+------------------CATEGORIAS------------------------------
+INSERT INTO CATEGORIAS VALUES ('FEUP', NULL);
+INSERT INTO CATEGORIAS VALUES ('SRSI', (SELECT REF(cc) FROM CATEGORIAS cc WHERE nome = 'FEUP'));
+INSERT INTO CATEGORIAS VALUES ('TBDA', (SELECT REF(cc) FROM CATEGORIAS cc WHERE nome = 'FEUP'));
+INSERT INTO CATEGORIAS VALUES ('Casa', NULL);
+INSERT INTO CATEGORIAS VALUES ('Supermercado', (SELECT REF(cc) FROM CATEGORIAS cc WHERE nome = 'Casa'));
+INSERT INTO CATEGORIAS VALUES ('Outros', (SELECT REF(cc) FROM CATEGORIAS cc WHERE nome = 'Casa'));
+INSERT INTO CATEGORIAS VALUES ('Trabalho', NULL);
 
-SELECT * FROM categorias;
-delete from categorias;
+SELECT * FROM CATEGORIAS;
+DELETE FROM CATEGORIAS;
+----------------------------------------------------------
 
-----------------CONTATOS--------------------------------
-INSERT INTO topicos VALUES (CONTATO( 
-    'João' -- titulo
-  , TIMESTAMP '2010-10-15 12:30:00' -- alteracao
-  , S_CATEGORIA((SELECT REF(cc) FROM categorias cc WHERE cc.nome = 'Trabalho'),
-                 (SELECT REF(cc) FROM categorias cc WHERE cc.nome = 'FEUP')) -- categorias
-  , NULL -- referencias
-  , 'João Abreu' -- nome
-  , NULL -- Telefone
-  , NULL -- Morada
-  , 'ja@example.com' -- email
-  , 'http://www.exemple.com/ja' -- url
+------------------CONTATOS--------------------------------
+INSERT INTO TOPICOS VALUES (CONTATO( 
+  'João', -- titulo
+  TIMESTAMP '2010-10-15 12:30:00', -- alteracao
+  S_CATEGORIA((SELECT REF(cc) FROM CATEGORIAS cc WHERE cc.nome = 'Trabalho'),
+    (SELECT REF(cc) FROM CATEGORIAS cc WHERE cc.nome = 'FEUP')), -- categorias
+  NULL, -- referencias
+  'João Abreu', -- nome
+  NULL, -- Telefone
+  NULL, -- Morada
+  'ja@example.com', -- email
+  'http://www.exemple.com/ja' -- url
 ));
 INSERT INTO topicos VALUES (CONTATO( 
-    'Maria' -- titulo
-  , TIMESTAMP '2010-10-15 12:30:00' -- alteracao
-  , S_CATEGORIA((SELECT REF(cc) FROM categorias cc WHERE cc.nome = 'FEUP')) -- categorias
-  , NULL -- referencias
-  , 'Maria Joana' -- nome
-  , NULL -- Telefone
-  , NULL -- Morada
-  , 'mj@example.com' -- email
-  , 'http://www.exemple.com/mj' -- url
+  'Maria', -- titulo
+  TIMESTAMP '2010-10-15 12:30:00', -- alteracao
+  S_CATEGORIA((SELECT REF(cc) FROM CATEGORIAS cc WHERE cc.nome = 'FEUP')), -- categorias
+  NULL, -- referencias
+  'Maria Joana', -- nome
+  NULL, -- Telefone
+  NULL, -- Morada
+  'mj@example.com', -- email
+  'http://www.exemple.com/mj' -- url
 ));
-----------------TAREFAS---------------------------------
+----------------------------------------------------------
+----- BUH ----
+------------------TAREFAS---------------------------------
 INSERT INTO topicos VALUES (TAREFA(
     'Comprar papel' -- titulo
   , TIMESTAMP '2010-10-15 12:30:00' -- alteracao
